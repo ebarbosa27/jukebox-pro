1ca2b688e5b691ce410ec4f65538055a35aee9a5
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-01 00:27:44 -0500

    added schema build to include user table and seeded that table with user data

--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,7 @@ import db from "#db/client";
 import { createPlaylist } from "#db/queries/playlists";
 import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { createTrack } from "#db/queries/tracks";
+import { createUser } from "#db/queries/users";
 
 await db.connect();
 await seed();
@@ -10,9 +11,13 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  for (let i = 1; i <= 20; i++) {
-    await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
-    await createTrack("Track " + i, i * 50000);
+
+  for (let j = 1; j <= 2; j++) {
+    const userData = await createUser("user " + j, "userPassword " + j);
+    for (let i = 1; i <= 20; i++) {
+      await createPlaylist("Playlist " + i, "lorem ipsum playlist description", userData.id);
+      await createTrack("Track " + i, i * 50000);
+    }
   }
   for (let i = 1; i <= 15; i++) {
     const playlistId = 1 + Math.floor(i / 2);
