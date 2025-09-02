fe141e11076231f7ead076c01b140a405bdd0cb7
ebarbosa27 <ebarbosa5449@gmail.com>
2025-09-01 21:58:47 -0500

    added track req to be able to search for playlist that i owned by user and contains a certain track

--- api/tracks.js
+++ api/tracks.js
@@ -2,7 +2,10 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { getTracks, getTrackById } from "#db/queries/tracks";
+import { getTracks, getTrackById, getPlaylistsByTrack } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
+
+router.use(requireUser);
 
 router.route("/").get(async (req, res) => {
   const tracks = await getTracks();
@@ -13,4 +16,10 @@ router.route("/:id").get(async (req, res) => {
   const track = await getTrackById(req.params.id);
   if (!track) return res.status(404).send("Track not found.");
   res.send(track);
+});
+
+router.route("/:id/playlists").get(async (req, res) => {
+  const playlists = await getPlaylistsByTrack(req.user.id, req.params.id);
+  if (playlists.length === 0) return res.status(404).send("Playlists not found.");
+  res.send(playlists);
 });
--- db/queries/tracks.js
+++ db/queries/tracks.js
@@ -47,3 +47,19 @@ export async function getTrackById(id) {
   } = await db.query(sql, [id]);
   return track;
 }
+
+export async function getPlaylistsByTrack(userId, trackId) {
+  // get all playlist that is owned by the user and that contains this track
+  const sql = `
+  SELECT
+    playlists.*
+  FROM
+    playlists
+    JOIN playlists_tracks ON playlists_tracks.playlist_id = playlists.id
+    JOIN tracks ON tracks.id = playlists_tracks.track_id
+  WHERE
+    playlists.user_id = $1 AND tracks.id = $2 
+  `;
+  const { rows: playlists } = await db.query(sql, [userId, trackId]);
+  return playlists;
+}
